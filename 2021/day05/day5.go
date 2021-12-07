228bf508ce416197a85a68ac6b7f08c090e0a3d6
Chris Hodges <chodges@dropbox.com>
2021-12-06 21:05:57 -0600

    Day 5 input parsing

--- 2021/day05/day5.go
+++ 2021/day05/day5.go
@@ -1,1 +1,44 @@
-package day05
+package main
+
+import (
+    "bufio"
+    "os"
+    "strconv"
+    "strings"
+)
+
+func main() {
+
+}
+
+func getInput(filename string) [][][]int {
+    listOfLines := make([][][]int, 0)
+
+    if f, err := os.Open(filename); err == nil {
+        defer f.Close()
+        scanner := bufio.NewScanner(f)
+
+        for scanner.Scan() {
+            raw := scanner.Text()
+            line := make([][]int, 0)
+            coords := strings.Split(raw, "->")
+            for _, coord := range coords {
+                points := make([]int, 0)
+                pointsRaw := strings.Split(coord, ",")
+                for _, point := range pointsRaw {
+                    num, err := strconv.Atoi(strings.TrimSpace(point))
+                    if err != nil {
+                        panic(err)
+                    }
+                    points = append(points, num)
+                }
+                line = append(line, points)
+            }
+            listOfLines = append(listOfLines, line)
+        }
+    } else {
+        panic("File error")
+    }
+
+    return listOfLines
+}
--- 2021/day05/day5_test.go
+++ 2021/day05/day5_test.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+    "github.com/stretchr/testify/assert"
+    "testing"
+)
+
+func TestGetInput(t *testing.T) {
+    expected := [][][]int {
+        {{0, 9}, {5,9}},
+        {{8, 0}, {0,8}},
+        {{9, 4}, {3,4}},
+        {{2, 2}, {2,1}},
+        {{7, 0}, {7,4}},
+        {{6, 4}, {2,0}},
+        {{0, 9}, {2,9}},
+        {{3, 4}, {1,4}},
+        {{0, 0}, {8,8}},
+        {{5, 5}, {8,2}},
+    }
+    assert.Equal(t, expected, getInput("test.txt"))
+}
